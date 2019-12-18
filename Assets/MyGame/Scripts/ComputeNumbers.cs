f1d23a54bfd4c2db405b0cedc5008d927dfd7918
verenazmo <verena.zmollnig@htl-salzburg.ac.at>
2019-12-18 11:24:39 +0100

    pythagoras

---
+++ Assets/MyGame/Scripts/ComputeNumbers.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ComputeNumbers : MonoBehaviour
+{
+    public Text result;
+    public InputField ipfVarA;
+    public InputField ipfVarB;
+    public Button btnSubNumbers;
+    public Button btnReset;
+
+    private void Start()
+    {
+        btnReset.interactable = false;
+    }
+
+    public void SubNumbersOnClick()
+    {
+        /*index++;
+        Debug.Log("onClick" + index);
+        Debug.Log("input Field A text is " + ipfVarA.text);
+        Debug.Log("input Field B text is " + ipfVarB.text);*/
+        int variableA = int.Parse(ipfVarA.text);
+        int variableB = int.Parse(ipfVarB.text);
+        int Subresult = variableA - variableB;
+        //Debug.Log("result " + (variableA - variableB));
+        result.text = Subresult.ToString();
+        ipfVarA.interactable = false;
+        ipfVarB.interactable = false;
+        btnSubNumbers.interactable = false;
+        btnReset.interactable = true;
+
+    }
+
+    public void ResetOnClick()
+    {
+        ipfVarA.interactable = true;
+        ipfVarB.interactable = true;
+        btnSubNumbers.interactable = true;
+        btnReset.interactable = false;
+
+        result.text = "Result";
+        ipfVarA.text = "0";
+        ipfVarB.text = "0";
+    }
+
+}
---
+++ Assets/MyGame/Scripts/Pythagoras.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pythagoras : MonoBehaviour
+{
+    public int value;
+    public float a;
+    public float b;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //int Result = Square(Value) + Square(Value);
+        //Debug.Log("result Addition:" + Result);
+        PythagorasMethod(a,b);
+        float result = PythagorasMethod(a, b) + PythagorasMethod(a, b);
+        Debug.Log("Result: " + result);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    int Square(int VarA)
+    {
+        Debug.Log(VarA * VarA);
+        return VarA * VarA;
+    }
+
+ 
+      private void OnValidate()
+    {
+        //Square(value);
+    }
+
+   
+     float PythagorasMethod(float a, float b)
+    {
+        float cSquare;
+        cSquare = (a * a) + (b * b);
+        float c = Mathf.Sqrt(cSquare);
+        Debug.Log("cSquare: " + cSquare);
+        Debug.Log("c: " + c);
+        return c;
+    }
+
+}
